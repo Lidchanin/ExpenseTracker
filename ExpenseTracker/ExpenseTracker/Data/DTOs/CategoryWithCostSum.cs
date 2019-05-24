5de1a00a1bc142582cb4a06f6d815b292a2cf6ec
Alexei Fokin <Zanuda.Lidchanin@gmail.com>
2019-05-24 09:40:56 +0300

    Added DTO for previous commit.

---
+++ ExpenseTracker/ExpenseTracker/Data/DTOs/CategoryWithCostSum.cs
@@ -0,0 +1,15 @@
+﻿namespace ExpenseTracker.Data.DTOs
+{
+    public class CategoryWithCostSum
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string HexColor { get; set; }
+
+        public string File { get; set; }
+
+        public double TotalSum { get; set; }
+    }
+}
